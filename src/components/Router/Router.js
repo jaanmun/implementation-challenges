745d78c66750e4464665cad952099e0c553e36e1
JaanMun <yjm7575@naver.com>
2023-12-27 18:26:20 +0900

    [feat] Add Router components

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import Todo from '../../pages/Todo/Todo';
+import Signup from '../../pages/Signup/Signup';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/sign-up" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
