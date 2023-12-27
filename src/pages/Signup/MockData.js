128d8a971d0a270d2cccfaf40813ee3fea738a81
JaanMun <yjm7575@naver.com>
2023-12-27 18:29:43 +0900

    [feat] Add mockdata utilized by signup components

---
+++ src/pages/Signup/MockData.js
@@ -0,0 +1,27 @@
+export const mockUserData = [
+  {
+    username: 'test1',
+    emailOrPhone: 'test1@example.com',
+    introduction: 'This is a mock introduction.',
+  },
+  {
+    username: 'test2',
+    emailOrPhone: 'test2@example.com',
+    introduction: 'This is a mock introduction.',
+  },
+  {
+    username: 'test3',
+    emailOrPhone: 'test3@example.com',
+    introduction: 'This is a mock introduction.',
+  },
+  {
+    username: 'test4',
+    emailOrPhone: 'test4@example.com',
+    introduction: 'This is a mock introduction.',
+  },
+  {
+    username: 'test5',
+    emailOrPhone: 'test5@example.com',
+    introduction: 'This is a mock introduction.',
+  },
+];
